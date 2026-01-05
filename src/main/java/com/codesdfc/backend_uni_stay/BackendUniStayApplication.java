210781723d7928b87ea9bcc1e6faf23ab9699a07
sdavdf <davidfueltala90@hotmail.com>
2026-01-05 14:23:00 -0500

    Frist

---
+++ src/main/java/com/codesdfc/backend_uni_stay/BackendUniStayApplication.java
@@ -0,0 +1,13 @@
+package com.codesdfc.backend_uni_stay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUniStayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUniStayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codesdfc/backend_uni_stay/BackendUniStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesdfc.backend_uni_stay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUniStayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
